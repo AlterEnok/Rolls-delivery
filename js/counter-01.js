b2387c6beccc0d7ba21bdd911793d9c753367c65
AlterEnok <ed.kestan@gmail.com>
2023-10-04 15:25:59 +0300

    update

--- js/counter-01.js
+++ js/counter-01.js
@@ -1,19 +1,25 @@
-// находим элементы на странице
+// Находим элементы на странице
 const btnMinus = document.querySelector('[data-action="minus"]');
-
 const btnPlus = document.querySelector('[data-action="plus"]');
-
 const counter = document.querySelector('[data-counter]');
 
-// отслеживаем клик на кнопку минус
-btnMinus.addEventListener('click', function () {
-  console.log('Minus click');
-
-  if (parseInt(counter.innerText) > 1) {
-    counter.innerText = --counter.innerText;
+// Функция для обработки события касания
+function handleTouch(event) {
+  // Проверяем тип события (touchstart для начала касания)
+  if (event.type === 'touchstart') {
+    // Ваш код обработки касания
+    if (event.target === btnMinus) {
+      console.log('Minus touch');
+      if (parseInt(counter.innerText) > 1) {
+        counter.innerText = --counter.innerText;
+      }
+    } else if (event.target === btnPlus) {
+      console.log('Plus touch');
+      counter.innerText = ++counter.innerText;
+    }
   }
-});
-// отслеживаем клик на кнопку плюс
-btnPlus.addEventListener('click', function () {
-  counter.innerText = ++counter.innerText;
-});
+}
+
+// Добавляем обработчики событий касания
+btnMinus.addEventListener('touchstart', handleTouch);
+btnPlus.addEventListener('touchstart', handleTouch);
